267b4bd10b0d1f5f2571d3e56348703b98b007a3
Madhur Dixit <dmadhur6@gmail.com>
2019-02-24 14:08:52 +0530

    Final Build

--- app/src/main/java/com/reallyinvincible/aura/activities/DisasterMapsActivity.java
+++ app/src/main/java/com/reallyinvincible/aura/activities/DisasterMapsActivity.java
@@ -1,33 +1,42 @@
 package com.reallyinvincible.aura.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
-
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.reallyinvincible.aura.AddDisasterBottomFragment;
 import com.reallyinvincible.aura.DialogueControlInterface;
-import com.reallyinvincible.aura.models.Information;
 import com.reallyinvincible.aura.R;
+import com.reallyinvincible.aura.models.Information;
 import com.reallyinvincible.aura.utils.UtilConstants;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
 public class DisasterMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,6 +46,9 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
     private ChildEventListener mChildEventListener;
     AddDisasterBottomFragment addDisasterBottomFragment;
     private static DialogueControlInterface dialogueControlInterface;
+    private static final String TAG = "DisasterMapsActivity";
+    HeatmapTileProvider mProvider;
+    List<LatLng> heatMapData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +58,8 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        heatMapData = new ArrayList<>();
+
         mDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mDatabase.getReference().child("DisasterInformation");
 
@@ -55,23 +69,47 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
                 Information disaster = dataSnapshot.getValue(Information.class);
                 String arr[] = UtilConstants.arr;
                 float color[] = UtilConstants.color;
+                int length = arr.length;
                 if (Arrays.asList(arr).indexOf(disaster.getAlertType()) > 0) {
                     BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory
                             .defaultMarker(color[Arrays.asList(arr).indexOf(disaster.getAlertType())]);
                     LatLng latLng = new LatLng(disaster.getLatitude(), disaster.getLongitude());
+                    heatMapData.add(latLng);
                     mMap.addMarker(new MarkerOptions()
                             .position(latLng)
                             .icon(bitmapDescriptor).title(disaster.getAlertType()));
                     moveCamera(latLng, 15f);
                 } else {
                     BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory
-                            .defaultMarker(color[6]);
+                            .defaultMarker(color[length]);
                     LatLng latLng = new LatLng(disaster.getLatitude(), disaster.getLongitude());
+                    heatMapData.add(latLng);
                     mMap.addMarker(new MarkerOptions()
                             .position(latLng)
                             .icon(bitmapDescriptor).title(disaster.getAlertType()));
                     moveCamera(latLng, 15f);
                 }
+
+                int[] colors = {
+                        Color.GREEN,
+                        Color.YELLOW,
+                        Color.RED
+                };
+
+                float[] startPoints = {
+                        0.1f, 0.8f, 1.5f
+                };
+
+                Gradient gradient = new Gradient(colors, startPoints);
+
+                mProvider = new HeatmapTileProvider.Builder()
+                        .data(heatMapData)
+                        .radius(40)
+                        .gradient(gradient)
+                        .opacity(0.7)
+                        .build();
+                // Add a tile overlay to the map, using the heat map tile provider.
+                TileOverlay mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
             }
 
             @Override
@@ -81,7 +119,6 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
             }
 
             @Override
@@ -95,19 +132,9 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
             }
         };
 
-        dialogueControlInterface = new DialogueControlInterface() {
-            @Override
-            public void dismiss() {
-                dismissDialogue();
-            }
-        };
+        dialogueControlInterface = () -> dismissDialogue();
 
-        findViewById(R.id.fab_add_disaster).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialogue();
-            }
-        });
+        findViewById(R.id.fab_add_disaster).setOnClickListener(view -> openDialogue());
 
     }
 
@@ -124,6 +151,19 @@ public class DisasterMapsActivity extends FragmentActivity implements OnMapReady
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
         mDatabaseReference.addChildEventListener(mChildEventListener);
     }
 
